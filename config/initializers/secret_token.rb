ddd09afbbce1a3cb7258b62785df74ab30a75722
SkySom <skylar.som@gmail.com>
2014-04-06 00:45:17 -0400

    Rails new HeadFirstRails
    Ran Rails to create all the bs it needs.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HeadFirstRails::Application.config.secret_key_base = '2bebe0be91641b370fe2b93bb980ad3c062122e800d2c5e695402853d411075c4fbb0364301759cb08eb105d4672a8cee18df4fefcbb75812719d1b1fa091169'
