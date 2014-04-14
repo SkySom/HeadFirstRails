53492492f569f4a239badd2df697fca1a4a22ce3
SkySom <skylar.som@gmail.com>
2014-04-13 21:29:02 -0400

    Added Phone to Tickets. Changed Jbuilders to be dealt with as text files as Intellij doesn't like them elsewise.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140406044926) do
+ActiveRecord::Schema.define(version: 20140406052451) do
 
   create_table "tickets", force: true do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140406044926) do
     t.string   "email_address"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "phone"
   end
 
 end
